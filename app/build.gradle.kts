1447b540de0f2e0d2ac55afa1e53116ac03adcbb
Yasasdev <yasaslekamge@gmail.com>
2025-02-20 22:54:43 +0530

    Multiple files update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.newsreporterapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.newsreporterapplication"
--- app/src/main/java/com/example/newsreporterapplication/fragments/EditNoteFragment.kt
+++ app/src/main/java/com/example/newsreporterapplication/fragments/EditNoteFragment.kt
@@ -3,17 +3,98 @@ package com.example.newsreporterapplication.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.newsreporterapplication.MainActivity
 import com.example.newsreporterapplication.R
+import com.example.newsreporterapplication.databinding.FragmentEditNoteBinding
+import com.example.newsreporterapplication.model.News
+import com.example.newsreporterapplication.viewmodel.NewsViewModel
 
-class EditNoteFragment : Fragment() {
+class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
+
+    private var editNoteBinding: FragmentEditNoteBinding? = null
+    private val binding get() = editNoteBinding!!
+
+    private lateinit var newsViewModel: NewsViewModel
+    private lateinit var currentNews: News
+
+    private val args: EditNoteFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_note, container, false)
+        editNoteBinding = FragmentEditNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        newsViewModel = (activity as MainActivity).newsViewModel
+        currentNews = args.news!!
+
+        binding.editNoteTitle.setText(currentNews.newsTitle)
+        binding.editNoteDesc.setText(currentNews.newsDescription)
+
+        binding.editNoteFab.setOnClickListener {
+            val noteTitle = binding.editNoteTitle.text.toString().trim()
+            val noteDesc = binding.editNoteDesc.text.toString().trim()
+
+            if (noteTitle.isNotEmpty()) {
+                val news = News(currentNews.id, noteTitle, noteDesc, "")
+                newsViewModel.updateNews(news)
+                view.findNavController().popBackStack(R.id.homeFragment2, false)
+            } else {
+                Toast.makeText(context, " Please Enter News title", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun deleteNote(){
+        AlertDialog.Builder(requireActivity()).apply {
+            setTitle("Delete Note")
+            setMessage("Are you sure you want to delete this news?")
+            setPositiveButton("Delete"){_,_ ->
+                newsViewModel.deleteNews(currentNews)
+                Toast.makeText(context, " News Deleted", Toast.LENGTH_SHORT).show()
+                view?.findNavController()?.popBackStack(R.id.homeFragment2, false)
+            }
+            setNegativeButton("Cancel", null)
+        }.create().show()
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menu.clear()
+        menuInflater.inflate(R.menu.menu_edit_note, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when(menuItem.itemId){
+            R.id.deleteMenu -> {
+                deleteNote()
+                true
+            } else -> false
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        editNoteBinding = null
     }
 }
